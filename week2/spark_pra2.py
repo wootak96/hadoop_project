e218cabf3db117fb6b86663bd2433841571024f5
wootak96 <90369173+wootak96@users.noreply.github.com>
2023-02-27 15:17:53 +0900

    Update spark_pra2.py

--- week2/spark_pra2.py
+++ week2/spark_pra2.py
@@ -9,7 +9,7 @@ def loadJob():
     with open("ds_salaries.text") as f:
         for line in f:
             fields = line.split(',')
-            if fields[4] not in Job_list.value():
+            if fields[4] not in Job_list.values():
                 cnt+=1
                 Job_list[cnt] = fields[4]
                 Job_id[fields[4]]=cnt
@@ -17,7 +17,7 @@ def loadJob():
     return Job_list
 
 def parseInput(line):
-    fields = line.split()
+    fields = line.split(',')
     return Row(JOBID = (fields[4]), salary = float(fields[5]))
 
 if __name__ == "__main__":
