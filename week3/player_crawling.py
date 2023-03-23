63624f3e5631b37c5f01491f0973a6dcd2dd5aa7
wootak96 <90369173+wootak96@users.noreply.github.com>
2023-03-23 21:02:34 +0900

    Create player_crawling.py

---
+++ week3/player_crawling.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+with open("C:/Users/신현준/Desktop/crawling/player.txt","w",encoding="utf8") as f:
+    for i in range(1,1550):
+        url="https://ygosu.com/reports/?m2=person&idx={}&newwindow=&search=YToxOntzOjExOiJzX2tleXdvcmRfdCI7czo0OiJuYW1lIjt9".format(i)
+        headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"}
+        res=requests.get(url,headers=headers)
+        res.raise_for_status()
+
+        soup=BeautifulSoup(res.text,"lxml")
+
+        player=soup.find_all("div",attrs={"class":"info"})
+        name=str(player)
+        name=name.split(" ")
+
+        if len(name)>=14:
+            f.write(str(i)+"   "+name[8]+"   "+name[13][0]+"\n") 
